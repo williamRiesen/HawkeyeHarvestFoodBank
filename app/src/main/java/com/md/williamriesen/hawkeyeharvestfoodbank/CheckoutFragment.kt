e3a62a2b342bf6e857cea1df94e2bac41f36e856
williamRiesen <william.riesen@gmail.com>
2020-10-11 07:41:51 -0500

    Formatting cleaned up; CheckoutFragment.kt back button implemented.

--- app/src/main/java/com/md/williamriesen/hawkeyeharvestfoodbank/CheckoutFragment.kt
+++ app/src/main/java/com/md/williamriesen/hawkeyeharvestfoodbank/CheckoutFragment.kt
@@ -1,11 +1,14 @@
 package com.md.williamriesen.hawkeyeharvestfoodbank
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.*
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
@@ -28,12 +31,17 @@ class CheckoutFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val checkoutView = inflater.inflate(R.layout.fragment_checkout, container, false)
+        val buttonBack = checkoutView.findViewById<Button>(R.id.buttonBack)
+        buttonBack.setOnClickListener { activity?.onBackPressed() }
         val recyclerView = checkoutView.findViewById<RecyclerView>(R.id.recyclerviewChoices)
         setUpRecyclerView(viewModel.itemList, recyclerView)
         return checkoutView
     }
 
-    private fun setUpRecyclerView(myList: MutableLiveData<MutableList<Item>>, recyclerView:RecyclerView) {
+    private fun setUpRecyclerView(
+        myList: MutableLiveData<MutableList<Item>>,
+        recyclerView: RecyclerView
+    ) {
         FirestoreRecyclerOptions.Builder<Item>()
         adapter = CheckoutAdapter(myList)
         recyclerView.layoutManager = LinearLayoutManager(this.activity)
