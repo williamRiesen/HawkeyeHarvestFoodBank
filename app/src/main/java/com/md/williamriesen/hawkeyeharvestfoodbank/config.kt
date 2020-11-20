f3bd435357501a3c04a695bf937730406a0ecdd4
williamRiesen <william.riesen@gmail.com>
2020-11-20 01:43:03 -0600

    About to try splitting signIn() into two parts; before and after firing activity or fragment to check if client on site.

--- app/src/main/java/com/md/williamriesen/hawkeyeharvestfoodbank/config.kt
+++ app/src/main/java/com/md/williamriesen/hawkeyeharvestfoodbank/config.kt
@@ -2,5 +2,5 @@ package com.md.williamriesen.hawkeyeharvestfoodbank
 
 const val openingHour24 = 13
 const val openingMinute = 0
-const val closingHour24 = 20
+const val closingHour24 = 22
 const val closingMinute = 0
