9e2496d01b18b68e15c5dd44b649254b37f894c9
Nicholas Riesen <nicholas.riesen@gmail.com>
2020-12-05 16:09:20 -0600

    refactor: Introduce Dagger into the application

--- app/src/main/java/com/md/williamriesen/hawkeyeharvest/config.kt
+++ app/src/main/java/com/md/williamriesen/hawkeyeharvest/config.kt
@@ -1,6 +1,16 @@
 package com.md.williamriesen.hawkeyeharvest
 
+import android.app.Application
+import dagger.Component
+
 const val openingHour24 = 8
 const val openingMinute = 45
 const val closingHour24 = 16
 const val closingMinute = 30
+
+@Component
+interface ApplicationComponent
+
+class HawkeyeHarvestFoodBankApplication : Application() {
+    val appComponent = DaggerApplicationComponent.create()
+}
